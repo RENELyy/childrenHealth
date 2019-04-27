af4ddd531d1d35c8861b4a388d861066f8a626d6
RENELyy <411192513@qq.com>
2019-04-27 22:43:50 +0800

    0427

---
+++ userRegister.php
@@ -0,0 +1,16 @@
+<?php
+include("coon.php");
+$nickname = $_GET['nickname'];
+$email = $_GET['email'];
+$password = $_GET['password'];
+$babyname = $_GET['babyname'];
+$babysex = $_GET['babysex'];
+$babybirthday = $_GET['babybirthday'];
+$guardianname = $_GET['guardianname'];
+$guardiansex = $_GET['guardiansex'];
+$guardianidcard = $_GET['guardianidcard'];
+$relationship = $_GET['relationship'];
+$phonenumber = $_GET['phonenumber'];
+$sql = "insert into 'user' (nickname,email,password,babyname,babysex,babybirthday,guardianname,guardiansex,guardianidcard,relationship,phonenumber) VALUES ('{$nickname}','{$email}','{$password}','{$babyname}','{$babysex}','{$babybirthday}','{$guardianname}','{$guardiansex}','{$guardianidcard}','{$relationship}','{$phonenumber}')";
+mysqli_query($link,$sql);
+?>
