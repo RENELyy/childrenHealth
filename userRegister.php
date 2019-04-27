971ab547b063c18bf5f4a554e9aa97d9c63a948e
RENELyy <411192513@qq.com>
2019-04-27 23:22:53 +0800

    0427

--- userRegister.php
+++ userRegister.php
@@ -1,5 +1,6 @@
 <?php
 include("coon.php");
+/*
 $nickname = $_GET['nickname'];
 $email = $_GET['email'];
 $password = $_GET['password'];
@@ -11,6 +12,18 @@ $guardiansex = $_GET['guardiansex'];
 $guardianidcard = $_GET['guardianidcard'];
 $relationship = $_GET['relationship'];
 $phonenumber = $_GET['phonenumber'];
+*/
+$nickname = 'nickname';
+$email = 'email';
+$password = 'password';
+$babyname = 'babyname';
+$babysex = 'babysex';
+$babybirthday = '2019-01-01';
+$guardianname = 'guardianname';
+$guardiansex = 'guardiansex';
+$guardianidcard = 'guardianidcard';
+$relationship = 'relationship';
+$phonenumber = '11';
 $sql = "insert into 'user' (nickname,email,password,babyname,babysex,babybirthday,guardianname,guardiansex,guardianidcard,relationship,phonenumber) VALUES ('{$nickname}','{$email}','{$password}','{$babyname}','{$babysex}','{$babybirthday}','{$guardianname}','{$guardiansex}','{$guardianidcard}','{$relationship}','{$phonenumber}')";
 mysqli_query($link,$sql);
 ?>
