a1ddc254f850e8292e08a6f95b193665d6b1e168
RENELyy <411192513@qq.com>
2019-04-27 20:20:40 +0800

    2019-4-27

--- insert.php
+++ insert.php
@@ -1,7 +1,6 @@
 <?php
 include("coon.php");
-$id = $_GET['id'];
 $name=$_GET['name'];
-$sql = "insert into `test`(id,name) VALUES ('{$id}','{$name}')";
+$sql = "insert into `test`(name) VALUES ('{$name}')";
 mysqli_query($link,$sql);
 >
