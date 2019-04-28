54b8162520f8db8dcefa40be1c3cba9eefe4e213
RENELyy <411192513@qq.com>
2019-04-28 08:13:33 +0800

    0428

--- userRegister.php
+++ userRegister.php
@@ -1,17 +1,17 @@
 <?php
 include("coon.php");
-$nickname = $_GET['nickname'];
+$username = $_GET['nickname'];
 $email = $_GET['email'];
 $password = $_GET['password'];
-$babyname = $_GET['babyname'];
-$babysex = $_GET['babysex'];
-$babybirthday = $_GET['babybirthday'];
-$guardianname = $_GET['guardianname'];
-$guardiansex = $_GET['guardiansex'];
-$guardianidcard = $_GET['guardianidcard'];
+$Bname = $_GET['babyname'];
+$Bsex = $_GET['babysex'];
+$Bbirthday = $_GET['babybirthday'];
+$Gname = $_GET['guardianname'];
+$Gsex = $_GET['guardiansex'];
+$Gidcard = $_GET['guardianidcard'];
 $relationship = $_GET['relationship'];
-$phonenumber = $_GET['phonenumber'];
-$sql = "insert into `user` (nickname,email,password,babyname,babysex,babybirthday,guardianname,guardiansex,guardianidcard,relationship,phonenumber) VALUES ('{$nickname}','{$email}','{$password}','{$babyname}','{$babysex}','{$babybirthday}','{$guardianname}','{$guardiansex}','{$guardianidcard}','{$relationship}','{$phonenumber}')";
+$phone = $_GET['phonenumber'];
+$sql = "insert into `user` (username,email,password,Bname,Bsex,Bbirthday,Gname,Gsex,GID,relationship,phone) VALUES ('{$username}','{$email}','{$password}','{$Bname}','{$Bsex}','{$Bbirthday}','{$Gname}','{$Gsex}','{$Gidcard}','{$relationship}','{$phone}')";
 mysqli_query($link,$sql);
 echo $sql;
 ?>
