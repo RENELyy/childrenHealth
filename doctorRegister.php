56d37188204f7b9e43112a35dfb88b43323452f7
RENELyy <411192513@qq.com>
2019-04-29 23:41:56 +0800

    0429

--- doctorRegister.php
+++ doctorRegister.php
@@ -1,16 +1,16 @@
 <?php
 include("coon.php");
-$username = $_Get['Dusername'];
-$email = $_Get['Demail'];
-$password = $_Get['Dpassword'];
-$hospital = $_Get['Dhospital'];
-$department = $_Get['Ddepartment'];
-$name = $_Get['Dname'];
-$sex = $_Get['Dsex'];
-$certificate = $_Get['Dcertificate'];
-$education = $_Get['Deducation'];
-$phone = $_Get['Dphone'];
-$sql = "insert into `doctor` (Dusername,Demail,Dpassword,Dhospital,Ddepartment,Dname,Dsex,Dcertificate,Deducation,Dphone) VALUES ('{$username}','{$email}','{$password}','{$hospital}','{$department}','{$name}','{$sex}','{$certificate}','{$education}','{$phone}')";
+$username = $_POST['username'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+$hospital = $_POST['hospital'];
+$department = $_POST['department'];
+$name = $_POST['name'];
+$sex = $_POST['sex'];
+$certificate = $_POST['certificate'];
+$education = $_POST['education'];
+$phone = $_POST['phone'];
+$sql = "insert into `user` (Dusername,Demail,Dpassword,Dhospital,Ddepartment,Dname,Dsex,Dcertificate,Deducation,Dphone) VALUES ('{$username}','{$email}','{$password}','{$hospital}','{$department}','{$name}','{$sex}','{$certificate}','{$education}','{$phone}')";
 mysqli_query($link,$sql);
 echo $sql;
 ?>
