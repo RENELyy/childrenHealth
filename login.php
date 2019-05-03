dadcac7bdf3b92a165380f8cf0365d5d39cb85aa
RENELyy <411192513@qq.com>
2019-05-03 10:51:37 +0800

    0503

--- login.php
+++ login.php
@@ -2,5 +2,14 @@
 include("coon.php");
 $sql = "select * from `user`";
 $result = mysqli_query($link,$sql);
+if(mysqli_num_rows($result) > 0 ) {
+	//输出数据
+	while($row = mysqli_fetch_assoc($result)) {
+		echo "id:" . $row["id"];
+	}
+} else {
+	echo "0 结果";
+}
+mysqli_close($link);
 each $result;
 ?>
