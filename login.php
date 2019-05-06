a00bb18235704312200e9179018c4399507b74e3
RENELyy <411192513@qq.com>
2019-05-06 17:05:11 +0800

    0506

--- login.php
+++ login.php
@@ -3,14 +3,20 @@ include("coon.php");
 $sql = "select * from `user`";
 $result = mysqli_query($link,$sql);
 if(mysqli_num_rows($result) > 0 ) {
-	//输出数据
-	while($row = mysqli_fetch_assoc($result)) {
-		echo "id:" . $row["id"];
-	}
-} else {
-	echo "0 结果";
+	$output[] = array('RnickName' => $row['RnickName'],
+            'id' => $row['id'],
+            'username' => $row['username'],
+            'email' => $row['email'],
+            'password' => $row['password'],
+            'Bname' => $row['Bname'],
+            'Bsex' => $row['Bsex'],
+            'Bbirthday' => $row['Bbirthday'],
+            'Gname' => $row['Gname'],
+            'Gsex' => $row['Gsex'],
+			'GID' => $row['GID'],
+			'relationship' => $row['relationship'],
+			'phone' => $row['phone']
+            );
 }
-mysqli_close($link);
-echo $result;
-echo "hello";
+print_r(json_encode($output));
 ?>
