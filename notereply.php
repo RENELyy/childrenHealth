e1154b1648be40e77ad6492b899f9908729ba982
RENELyy <411192513@qq.com>
2019-06-02 22:30:34 +0800

    0602

---
+++ notereply.php
@@ -0,0 +1,11 @@
+<?php
+include ('coon.php');
+$sql = "select * from `reply`";
+$result = mysqli_query($link,$sql);
+if (mysqli_num_rows($result) > 0) {
+    while ($row = mysqli_fetch_assoc($result)) {
+        $output[] = array('id' => $row['id'],'nickName' => $row['nickName'],'replyname' => $row['name'],'replycontent' => $row['content'],'replytodaytime' => $row['todaytime'],'replyview' => $row['view'],'replyimg' => $row['replyimg']);
+    }
+}
+print_r(json_encode($output));
+?>
