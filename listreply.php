4dd94d9a5ef7606be444f03735cdaae1eafe9d6d
RENELyy <411192513@qq.com>
2019-06-03 12:43:00 +0800

    0603

--- listreply.php
+++ listreply.php
@@ -3,10 +3,11 @@ include ("coon.php");
 $nickName = $_GET['nickName'];
 $nickNameImg = $_GET['nickNameImg'];
 $replyname = $_GET['replyname'];
+$replydate = $_GET['replydate'];
 $replytime = $_GET['replytime'];
 $replycontent = $_GET['replycontent'];
 $view = $_GET['view'];
 $replyimg = $_GET['replyimg'];
-$sql = "insert into `reply` (nickName,nickNameImg,replyname,replycontent,replytime,view,replyimg) VALUES ('{$nickName}','{$nickNameImg}','{$replyname}','{$replycontent}','{$replytime}','{$view}','{$replyimg}')";
+$sql = "insert into `reply` (nickName,nickNameImg,replyname,replycontent,replydate,replytime,view,replyimg) VALUES ('{$nickName}','{$nickNameImg}','{$replyname}','{$replycontent}','{$replydate}','{$replytime}','{$view}','{$replyimg}')";
 mysqli_query($link,$sql);
 ?>
