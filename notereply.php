dd9b95baa94b44b41c535150804cb30051368637
RENELyy <411192513@qq.com>
2019-06-03 12:18:24 +0800

    0603

--- notereply.php
+++ notereply.php
@@ -4,7 +4,7 @@ $sql = "select * from `reply`";
 $result = mysqli_query($link,$sql);
 if (mysqli_num_rows($result) > 0) {
     while ($row = mysqli_fetch_assoc($result)) {
-        $output[] = array('id' => $row['id'],'nickName' => $row['nickName'],'replyname' => $row['name'],'replycontent' => $row['content'],'replytodaytime' => $row['todaytime'],'replyview' => $row['view'],'replyimg' => $row['replyimg']);
+        $output[] = array('id' => $row['id'],'nickName' => $row['nickName'],'replyname' => $row['name'],'replycontent' => $row['content'],'replytodaytime' => $row['todaytime'],'replyview' => $row['view'],'replyimg' => $row['replyimg'],'nickNameImg' => $row['nickNameImg']);
     }
 }
 print_r(json_encode($output));
